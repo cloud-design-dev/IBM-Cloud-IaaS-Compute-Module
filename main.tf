5e383a01796af17768a036f27acdb3bbf9c67c8e
greyhoundforty <ryantiffany@fastmail.com>
2019-04-01 12:23:24 -0500

    Fixed main.tf

--- main.tf
+++ main.tf
@@ -7,23 +7,16 @@ resource "random_id" "name" {
 }
 
 resource "ibm_compute_vm_instance" "node" {
-
-  hostname                  = "tf-rt-${random_id.name.hex}"
-  domain                    = "${var.domainname}"
-  user_metadata             = "${file("${path.module}/install.yml")}"
-  os_reference_code         = "${var.os}"
+  hostname                  = "${random_id.name.hex}-tf"
+  domain                    = "${var.domain}"
+  os_reference_code         = "${var.os_reference_code}"
   datacenter                = "${var.datacenter}"
   network_speed             = 1000
   hourly_billing            = true
   private_network_only      = false
-  flavor_key_name           = "${var.vm_flavor}"
+  flavor_key_name           = "${var.flavor_key_name}"
   disks                     = [200]
   local_disk                = false
-  public_vlan_id            = "${var.pub_vlan}"
-  private_vlan_id           = "${var.priv_vlan}"
-  ssh_key_ids               = ["${data.ibm_compute_ssh_key.deploymentKey.id}"]
-
-  tags = [
-    "ryantiffany",
-  ]
+  public_vlan_id            = "${var.public_vlan_id}"
+  private_vlan_id           = "${var.private_vlan_id}"
 }
