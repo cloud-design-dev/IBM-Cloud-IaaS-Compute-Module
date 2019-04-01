949440df63a2f8f78d550458ace0aea6a569d554
greyhoundforty <ryantiffany@fastmail.com>
2019-04-01 12:26:46 -0500

    Ran terraform fmt

--- main.tf
+++ main.tf
@@ -7,16 +7,16 @@ resource "random_id" "name" {
 }
 
 resource "ibm_compute_vm_instance" "node" {
-  hostname                  = "${random_id.name.hex}-tf"
-  domain                    = "${var.domain}"
-  os_reference_code         = "${var.os_reference_code}"
-  datacenter                = "${var.datacenter}"
-  network_speed             = 1000
-  hourly_billing            = true
-  private_network_only      = false
-  flavor_key_name           = "${var.flavor_key_name}"
-  disks                     = [200]
-  local_disk                = false
-  public_vlan_id            = "${var.public_vlan_id}"
-  private_vlan_id           = "${var.private_vlan_id}"
+  hostname             = "${random_id.name.hex}-tf"
+  domain               = "${var.domain}"
+  os_reference_code    = "${var.os_reference_code}"
+  datacenter           = "${var.datacenter}"
+  network_speed        = 1000
+  hourly_billing       = true
+  private_network_only = false
+  flavor_key_name      = "${var.flavor_key_name}"
+  disks                = [200]
+  local_disk           = false
+  public_vlan_id       = "${var.public_vlan_id}"
+  private_vlan_id      = "${var.private_vlan_id}"
 }
--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable ibm_sl_username {}
 variable ibm_sl_api_key {}
 
 variable datacenter {
-  type = "string"
+  type    = "string"
   default = ""
 }
 
@@ -14,22 +14,22 @@ variable node_count {
 }
 
 variable os_reference_code {
-  type = "string"
+  type    = "string"
   default = ""
 }
 
 variable flavor_key_name {
-  type = "string"
+  type    = "string"
   default = ""
 }
 
 variable private_vlan_id {
-  type = "string"
+  type    = "string"
   default = ""
 }
 
 variable public_vlan_id {
-  type = "string"
+  type    = "string"
   default = ""
 }
 
