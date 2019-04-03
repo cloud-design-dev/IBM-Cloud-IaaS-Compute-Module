c1914b9ffd740f96c1fe9a808d2cad5366a6bb14
greyhoundforty <ryantiffany@fastmail.com>
2019-04-03 08:40:24 -0500

    Omiited ipv6 for now

--- outputs.tf
+++ outputs.tf
@@ -26,13 +26,6 @@ output "ibm_vm_private_subnet_id" {
   value = "${ibm_compute_vm_instance.node.*.private_subnet_id}"
 }
 
-output "ibm_vm_ipv6_address" {
-  value = "${ibm_compute_vm_instance.node.*.ipv6_address}"
-}
-
-output "ibm_vm_ipv6_address_id" {
-  value = "${ibm_compute_vm_instance.node.*.ipv6_address_id}"
-}
 
 output "ibm_vm_secondary_ip_addresses" {
   value = "${jsonencode(ibm_compute_vm_instance.node.*.secondary_ip_addresses)}"
