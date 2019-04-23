74b1a1f84c23bac906ba97ee2c52838eda7524d9
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2019-04-23 08:50:29 -0500

    Checking nill response for VLAN options

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ resource "ibm_compute_vm_instance" "node" {
   private_network_only = false
   user_metadata        = "${var.user_metadata}"
   flavor_key_name      = "${var.flavor_key_name}"
-  disks                = [200]
+  disks                = "${var.disks}"
   local_disk           = "${var.localdisk}"
-#  public_vlan_id       = "${var.public_vlan_id}"
-#  private_vlan_id      = "${var.private_vlan_id}"
+  public_vlan_id       = "${var.public_vlan_id}"
+  private_vlan_id      = "${var.private_vlan_id}"
 }
