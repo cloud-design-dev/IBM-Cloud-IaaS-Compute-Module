00850a25aa677337f9b324799cd317b292953607
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-01-28 19:11:20 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 resource "ibm_compute_vm_instance" "node" {
-  transient            = "${var.transient}"
-  count                = "${var.node_count}"
-  hostname             = "${var.hostname}-${count.index+1}"
-  domain               = "${var.domain}"
-  os_reference_code    = "${var.os_reference_code}"
-  datacenter           = "${var.datacenter}"
+  transient            = var.transient
+  hostname             = var.hostname
+  domain               = var.domain
+  os_reference_code    = var.os_reference_code
+  datacenter           = var.datacenter
   network_speed        = 1000
   hourly_billing       = true
   private_network_only = false
-  user_metadata        = "${var.user_metadata}"
-  flavor_key_name      = "${var.flavor_key_name}"
-  local_disk           = "${var.localdisk}"
+  user_metadata        = var.user_metadata
+  flavor_key_name      = var.flavor_key_name
+  local_disk           = var.localdisk
 }
